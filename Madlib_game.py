e48d045d3a8b65e63c15371eb5276b2c90f2ab07
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-11-30 22:41:43 -0300

    Add Mad Lib game implementation for interactive storytelling

---
+++ Madlib_game.py
@@ -0,0 +1,19 @@
+print("Bem-vindo (a) ao 🎮 Mad Lib: Uma Aventura no Espaço! ")
+print()
+nomeProprio = input("Digite um nome próprio: ")
+planeta = input("Digite o nome de um planeta: ")
+animal = input("Digite o nome de um animal: ")
+encontraNaCozinha = input("Digite o nome de um objeto que você encontra na cozinha: ")
+verboNoPassado = input("Digite um verbo no passado: ")
+adjetivo = input("Digite um adjetivo: ")
+comida = input("Digite o nome de uma comida: ")
+numero = input("Digite um número: ")
+lugar = input("Digite o nome de um lugar: ")
+
+print("\nAqui está a sua história:\n")
+
+print(f"""O astronauta {nomeProprio} estava viajando pelo espaço quando pousou no planeta {planeta}.
+Lá, encontrou um gigante {animal} que segurava {encontraNaCozinha}.
+O astronauta {verboNoPassado} de susto, mas logo percebeu que o animal era muito {adjetivo}.
+Eles se tornaram amigos e comeram {comida} juntos.
+Depois de {numero} horas explorando, os dois voltaram para {lugar} para contar essa grande aventura!""")
