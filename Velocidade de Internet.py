02574eeaa1a00d3a0ee5f8fe18c00e7d7b4b77dc
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-12-20 13:27:23 -0300

    Enhance output formatting for internet speed results

--- Velocidade de Internet.py
+++ Velocidade de Internet.py
@@ -13,6 +13,6 @@ up_speed_mbps = up_speed / 1_000_000
 
 # Exibindo os resultados
 
-print(f"Velocidade de Download: {down_speed_mbps:.2f} Mbps")
-print(f"Velocidade de Upload: {up_speed_mbps:.2f} Mbps")
+print(f"Velocidade de Download ⬇️: {down_speed_mbps:.2f} Mbps")
+print(f"Velocidade de Upload ⬆️: {up_speed_mbps:.2f} Mbps")
 print(f"Ping: {ping:.2f} ms")
