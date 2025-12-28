766beb7169ec17790b090653155f1e1878c259d6
Alysson Ribeiro <alyssonmelo.ti@gmail.com>
2025-12-27 22:58:26 -0300

    Add implementation for Mega-Sena probability calculation

--- listas_tuplas.py
+++ listas_tuplas.py
@@ -8,5 +8,18 @@ def listar_tarefas(tarefa):
     for tarefa in tarefas:
         print(f"Tarefa: {tarefa[0]} - Status: {tarefa[1]}")
 
+def concluir_tarefa(tarefa):
+    global tarefas
+    tarefas = [ (t[0], "Concluída") if t[0] == tarefa else t for t in tarefas]
+
 adicionar_tarefa("Arrumar o quarto")
+adicionar_tarefa("Estudar para a prova")
+listar_tarefas(tarefas)
+
+print()
+
+print("Agora vamos começar a concluir algumas tarefas...\n")
+
+concluir_tarefa("Arrumar o quarto")
+concluir_tarefa("Estudar para a prova")
 listar_tarefas(tarefas)
---
+++ megaSena.py
@@ -0,0 +1,19 @@
+#Cálculo da probabilidade de ganhar na Mega-Sena com um número específico de jogos simples.
+
+import math
+
+def combinacoes(n, k):
+    return math.comb(n, k)
+
+numeros_totais = 60
+numeros_escolhidos = 6
+quantidade_jogos = 1  # número de jogos simples
+
+total_combinacoes = combinacoes(numeros_totais, numeros_escolhidos)
+
+# chance considerando vários jogos
+chance = quantidade_jogos / total_combinacoes
+
+print(f'Total de combinações possíveis: {total_combinacoes:,}'.replace(',', '.'))
+print(f'Chance de ganhar com {quantidade_jogos} jogos: 1 em {(total_combinacoes / quantidade_jogos):,.0f}'.replace(',', '.'))
+print(f'Probabilidade percentual: {chance * 100:.8f}%')
